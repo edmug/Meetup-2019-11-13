030f643af38172651b9a2f5d857fbe74ae6d3064
edmugdotnet <57768420+edmugdotnet@users.noreply.github.com>
2019-11-19 09:44:34 -0700

    Add files via upload

---
+++ BlazorIntro/Blazor.Server/Data/EventLogService.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Blazor.Shared;
+
+namespace Blazor.Server.Data
+{
+    public class EventLogService
+    {
+        private static int _id;
+        private static readonly List<EventLog> Logs = new List<EventLog>();
+
+        private static readonly string[] EventLogTypes = {
+            "Trace", "Debug", "Information", "Warning", "Error"
+        };
+
+        public async Task<EventLog[]> GetEventLogsAsync(int cacheSize = 10)
+        {
+            var rng = new Random();
+
+            var logs = await Task.FromResult(Enumerable.Range(1, 5)
+                .Select(index => CreateLogEvent(
+                    EventLogTypes[rng.Next(EventLogTypes.Length)],
+                    rng.Next(1, 50)))
+                    .ToArray());
+
+            Logs.AddRange(logs);
+
+            while (Logs.Count > cacheSize) 
+                Logs.RemoveAt(0);
+
+            return Logs.OrderByDescending(e => e.Id).TakeLast(cacheSize).ToArray();
+        }
+        private static EventLog CreateLogEvent(string eventType, int processId)
+        {
+            return new EventLog
+            {
+                Id = ++_id,
+                LoggedAt = DateTime.UtcNow,
+                LogType = eventType,
+                ProcessId = processId,
+                Message = CreateMessage(eventType)
+            };
+        }
+
+        private static string CreateMessage(string eventType)
+        {
+            try
+            {
+                if (eventType == "ERR")
+                {
+                    throw new Exception($"A message that was logged as '{eventType}'.");
+                }
+
+                return $"A message that was logged as '{eventType}'.";
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                return ex.ToString();
+            }
+        }
+    }
+}
---
+++ BlazorIntro/Blazor.Server/Data/EventLogWorker.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Blazor.Server.Data
+{
+    public class EventLogWorker : BackgroundService
+    {
+        private readonly ILogger<EventLogWorker> _logger;
+        private readonly IMemoryCache _cache;
+        private readonly EventLogService _service;
+
+        public EventLogWorker(ILogger<EventLogWorker> logger, IMemoryCache cache, EventLogService service)
+        {
+            _logger = logger;
+            _cache = cache;
+            _service = service;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                try
+                {
+                    _cache.TryGetValue<int>(EventLogWorkerConstants.EventLogCacheSize, out var cacheSize);
+                    if (cacheSize < 1) cacheSize = 10;
+                    if (cacheSize > 100) cacheSize = 100;
+
+                    _cache.TryGetValue<int>(EventLogWorkerConstants.EventLogCacheDelay, out var cacheDelay);
+                    if (cacheDelay == 0) cacheDelay = 3;
+                    if (cacheDelay < 1) cacheDelay = 1;
+                    if (cacheDelay > 60) cacheDelay = 60;
+
+                    var items = await _service.GetEventLogsAsync(cacheSize);
+                    _cache.Set(EventLogWorkerConstants.EventLogCache, items);
+
+                    await Task.Delay(TimeSpan.FromSeconds(cacheDelay), stoppingToken);
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError("Unexpected error fetching event log data: {ex}", ex);
+                }
+
+                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+            }
+        }
+    }
+    public static class EventLogWorkerConstants
+    {
+        public const string EventLogCacheSize = "EventLogCacheSize";
+        public const string EventLogCacheDelay = "EventLogCacheDelay";
+        public const string EventLogCache = "EventLogCache";
+    }
+}
